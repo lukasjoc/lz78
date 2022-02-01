6841b658f0544f08aad321e1f4a571a52ccb7a86
lukasjoc <jochamlu@gmail.com>
2022-02-01 23:53:26 +0100

    work on some fixes for large encode/decode like zarathustra

--- lz78.py
+++ lz78.py
@@ -4,6 +4,7 @@ LZ78 compression (Simplified)
 """
 
 from dataclasses import dataclass
+import sys
 
 # The `Chunk` Type for a given pair
 
@@ -55,17 +56,22 @@ def decoder(phrase):
         ddk = list(dd.keys())
         if l.isnumeric():
             ip += l
+            print(ip)
         else:
+            print(ap)
             index += 1
             ap += l
 
-            ap = str(ddk[int(ip)]) + ap if int(ip) > 0 else ap
-            c = Chunk(ref=int(ip),
-                      frag=str(ap),
-                      index=index)
+            if len(ip) > 0:
+                if int(ip) > 0:
+                    ap = str(ddk[int(ip)])+ap
 
-            dd[ap] = c
-            o += ap
+                c = Chunk(ref=int(ip),
+                          frag=str(ap),
+                          index=index)
+
+                dd[ap] = c
+                o += ap
             ip = ""
             ap = ""
 
@@ -74,20 +80,26 @@ def decoder(phrase):
 
     return o
 
-
 if __name__ == "__main__":
-    assert encoder('ABAABABAABAB') == '0A0B1A2A4A4B'
-    assert encoder('ABAABABAABABAA') == '0A0B1A2A4A4B3'
-    assert encoder('ABBCBCABABCAABCAABBCAA') == '0A0B2C3A2A4A6B6'
-    assert encoder('AAAAAAAAAAAAAAA') == '0A1A2A3A4A'
-    assert encoder('ABCABCABCABCABCABC') == '0A0B0C1B3A2C4C7A6'
-    assert encoder(
-        'ABCDDEFGABCDEDBBDEAAEDAEDCDABC') == '0A0B0C0D4E0F0G1B3D0E4B2D10A1E4A10D9A2C'
+    data = ""
+    for line in sys.stdin:
+        data += line
 
-    assert decoder('0A0B1A2A4A4B') == 'ABAABABAABAB'
-    assert decoder('0A0B1A2A4A4B3') == 'ABAABABAABABAA'
-    assert decoder('0A0B2C3A2A4A6B6') == 'ABBCBCABABCAABCAABBCAA'
-    assert decoder('0A1A2A3A4A') == 'AAAAAAAAAAAAAAA'
-    assert decoder('0A0B0C1B3A2C4C7A6') == 'ABCABCABCABCABCABC'
-    assert decoder(
-        '0A0B0C0D4E0F0G1B3D0E4B2D10A1E4A10D9A2C') == 'ABCDDEFGABCDEDBBDEAAEDAEDCDABC'
+    # print(encoder(data))
+    print(decoder(data))
+
+#if __name__ == "__main__":
+#    assert encoder('ABAABABAABAB') == '0A0B1A2A4A4B'
+#    assert encoder('ABAABABAABABAA') == '0A0B1A2A4A4B3'
+#    assert encoder('ABBCBCABABCAABCAABBCAA') == '0A0B2C3A2A4A6B6'
+#    assert encoder('AAAAAAAAAAAAAAA') == '0A1A2A3A4A'
+#    assert encoder('ABCABCABCABCABCABC') == '0A0B0C1B3A2C4C7A6'
+#    assert encoder(
+#        'ABCDDEFGABCDEDBBDEAAEDAEDCDABC') == '0A0B0C0D4E0F0G1B3D0E4B2D10A1E4A10D9A2C'
+#
+#    assert decoder('0A0B1A2A4A4B') == 'ABAABABAABAB'
+#    assert decoder('0A0B1A2A4A4B3') == 'ABAABABAABABAA'
+#    assert decoder('0A0B2C3A2A4A6B6') == 'ABBCBCABABCAABCAABBCAA'
+#    assert decoder('0A1A2A3A4A') == 'AAAAAAAAAAAAAAA'
+#    assert decoder('0A0B0C1B3A2C4C7A6') == 'ABCABCABCABCABCABC'
+#    assert decoder('0A0B0C0D4E0F0G1B3D0E4B2D10A1E4A10D9A2C') == 'ABCDDEFGABCDEDBBDEAAEDAEDCDABC'
