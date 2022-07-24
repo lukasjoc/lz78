f7ca6c125813d25f631100bb68f29cce7c1a7c1b
lukasjoc <jochamlu@gmail.com>
2022-07-24 12:39:45 +0200

    add some formatting

--- lz78.py
+++ lz78.py
@@ -1,6 +1,5 @@
 """
 LZ78 compression (Simplified)
-
 """
 from dataclasses import dataclass
 import sys
@@ -28,8 +27,8 @@ def encoder(phrase):
         if not temp in ddk:
             index += 1
             c = Chunk(ref=ddk.index(mem),
-                    frag=temp[len(temp) - 1],
-                    index=index)
+                      frag=temp[len(temp) - 1],
+                      index=index)
 
             dd[temp] = c
             o += str(c.ref) + c.frag
@@ -43,6 +42,7 @@ def encoder(phrase):
 
     return o
 
+
 def decoder(phrase):
     index = 0
     dd = {"": Chunk(ref=0, frag="", index=index)}
@@ -54,22 +54,22 @@ def decoder(phrase):
             if str(value.index) == str(ref):
                 f = value.frag + f
                 if (len(str(value.ref)) > 0
-                    and int(value.ref) > 0):
+                        and int(value.ref) > 0):
                     return get_frag_by_ref(str(value.ref), f)
         return f
 
     for l in phrase:
         temp += l
         ddk = (list(dd.keys()) + [str(c.ref) for c in dd.values()]
-                              + [str(c.index) for c in dd.values()])
+               + [str(c.index) for c in dd.values()])
         if not temp in ddk:
             index += 1
             index_len = len(str(index))
             if index_len >= len(temp):
                 index_len = len(temp) - 1
             c = Chunk(ref=temp[:index_len],
-                    frag=temp[len(temp) - 1],
-                    index=index)
+                      frag=temp[len(temp) - 1],
+                      index=index)
             dd[temp] = c
             o += get_frag_by_ref(c.ref) + c.frag
             temp = ""
@@ -80,5 +80,3 @@ def decoder(phrase):
             if str(value.index) == str(temp):
                 o += get_frag_by_ref(value.index)
     return o
-
-
